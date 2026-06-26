39fcf60406bf3c024f08ac206e083b8d4555e5de
ADB-DevWorks <adam.brawley@snhu.edu>
2026-06-26 08:39:28 -0500

    Added CRUD operations

--- MyGroceryList/Data/AppDatabase.cs
+++ MyGroceryList/Data/AppDatabase.cs
@@ -15,5 +15,30 @@ namespace MyGroceryList.Data
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<GroceryItem>().Wait();
         }
+
+        // Loads items in correct order
+        public Task<List<GroceryItem>> GetItemsAsync()
+        {
+            return _database.Table<GroceryItem>()
+                .OrderBy(i => i.SortOrder)
+                .ToListAsync();
+        }
+
+        // Add new item
+        public Task<int> AddItemAsync(GroceryItem item)
+        {
+            return _database.InsertAsync(item);
+        }
+
+        public Task<int> UpdateItemAsync(GroceryItem item)
+        {
+            return _database.UpdateAsync(item);
+        }
+
+        public Task<int> DeleteItemAsync(GroceryItem item)
+        {
+            return _database.DeleteAsync(item);
+        }
+
     }
 }
