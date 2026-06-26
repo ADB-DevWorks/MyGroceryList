2c2042447e6dfd8be5f02cbd17d2eaef5a1fb281
ADB-DevWorks <adam.brawley@snhu.edu>
2026-06-26 08:24:10 -0500

    installed database and created class

---
+++ MyGroceryList/Data/AppDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using MyGroceryList.Models;
+
+namespace MyGroceryList.Data
+{
+    public class AppDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<GroceryItem>().Wait();
+        }
+    }
+}
