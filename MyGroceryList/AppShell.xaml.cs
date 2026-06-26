a910822897bbfdbd5bbdf6111e33136d0f2828d0
ADB-DevWorks <adam.brawley@snhu.edu>
2026-06-26 08:15:36 -0500

    Add project files.

---
+++ MyGroceryList/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyGroceryList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyGroceryList/Models/GroceryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGroceryList.Models
+{
+    public class GroceryItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
